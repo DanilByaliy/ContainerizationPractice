098dbaf8309d90d24f112fe5ee24cca79471eee9
DanilByaliy <byaliy03@gmail.com>
2022-05-21 19:30:26 +0300

    Add dependency, endpoint, change Dockerfile
    Added numpy dependency to requirements.txt and backend.in. Added an
    endpoint that acts on and outputs matrices. In Dockerfile, I changed
    the base image to python: 3.10-alpine and added a command line to
    solve the problem.

--- project-python/spaceship/routers/api.py
+++ project-python/spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
@@ -6,3 +7,16 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+
+@router.get('/matrices')
+def create_matrices() -> dict:
+    first_matrix = np.random.rand(10, 10)
+    second_matrix = np.random.rand(10, 10)
+    product = np.matmul(first_matrix, second_matrix)
+    
+    return {
+        'matrix_a': first_matrix.tolist(),
+        'matrix_b': second_matrix.tolist(),
+        'product': product.tolist()
+        }
